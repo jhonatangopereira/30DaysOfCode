a1c50da84b0af3c8a5460c77fb9bbc444f39be2f
Jhonatan Pereira <jhonatangopereira@gmail.com>
2022-12-08 15:12:15 -0300

    feat (day-10): Binary numbers problem solved

--- day10.cpp
+++ day10.cpp
@@ -1,3 +1,12 @@
+// Day 10: Binary Numbers
+
+/*
+Given a base-10 integer, n, convert it to binary (base-2). Then find and print the
+base-10 integer denoting the maximum number of consecutive 1's in n's binary
+representation. When working with different bases, it is common to show the base
+as a subscript.
+*/
+
 #include <bits/stdc++.h>
 #include <bitset>
 
